f99d304178ee1eb99322d6c15767b478d4e26b1c
J <wjdgusrl02@gmail.com>
2025-11-28 02:14:48 +0900

    Fix: Finalize casing and address local changes before pull

--- src/page/translate.js
+++ src/page/translate.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { base44 } from '../api/base44Client';
-import { Button } from "../components/ui/button";
+import { Button } from "../components/ui/Button";
 import { Textarea } from "../components/ui/textarea";
 import { 
   Languages, ArrowRightLeft, Loader2, Copy, Check, 
